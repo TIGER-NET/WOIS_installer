4b62f4105d490997c40cb7fea7695d5362366183
Jonas <josl@dhi-gras.com>
2017-01-26 15:27:34 +0100

    update to SNAP5

--- woisInstaller.py
+++ woisInstaller.py
@@ -65,7 +65,7 @@ class woisInstaller():
                 installationsDir = "Installations_x32"
                 osgeo4wInstall = os.path.join(installationsDir, "osgeo4w-setup.bat")
                 beamInstall = os.path.join(installationsDir, "beam_5.0_win32_installer.exe")
-                snapInstall = os.path.join(installationsDir, "esa-snap_sentinel_windows_3_0.exe")
+                snapInstall = os.path.join(installationsDir, "esa-snap_sentinel_windows_5_0.exe")
                 rInstall = os.path.join(installationsDir, "R-3.1.3-win.exe")
                 postgreInstall = os.path.join(installationsDir, "postgresql-9.3.6-2-windows.exe")
                 postgisInstall = os.path.join(installationsDir, "postgis-bundle-pg93x32-setup-2.1.5-1.exe")
@@ -77,7 +77,7 @@ class woisInstaller():
                 installationsDir = "Installations_x64"
                 osgeo4wInstall = os.path.join(installationsDir, "osgeo4w-setup.bat")
                 beamInstall = os.path.join(installationsDir, "beam_5.0_win64_installer.exe")
-                snapInstall = os.path.join(installationsDir, "esa-snap_sentinel_windows-x64_3_0.exe")
+                snapInstall = os.path.join(installationsDir, "esa-snap_sentinel_windows-x64_5_0.exe")
                 rInstall = os.path.join(installationsDir, "R-3.1.3-win.exe")
                 postgreInstall = os.path.join(installationsDir, "postgresql-9.3.6-2-windows-x64.exe")
                 postgisInstall = os.path.join(installationsDir, "postgis-bundle-pg93x64-setup-2.1.5-2.exe")
@@ -148,6 +148,7 @@ class woisInstaller():
             srcPath = os.path.join("QGIS WOIS plugins", "plugins.zip")
             # try to delete old plugins before copying the new ones to avoid conflicts
             plugins_to_delete = [
+                'mikecprovider',
                 'processing',
                 'processing_gpf',
                 'photo2shape',
